44c9ddd3633a9f104c8aeaf57c4daf79397d45b0
morennovela21 <114722185+morennovela21@users.noreply.github.com>
2023-01-24 02:32:54 +0800

    commit

--- movie1/scripts.js
+++ movie1/scripts.js
@@ -394,17 +394,4 @@ function pageCall(page) {
     getMovies(url);
   }
 }
-// TOGGLE
-
-const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(
-  ".container,.movie-list-title,.navbar-container,.sidebar,.toggle"
-);
-
-ball.addEventListener("click", () => {
-  items.forEach((item) => {
-    item.classList.toggle("active");
-  });
-  ball.classList.toggle("active");
-});
 
